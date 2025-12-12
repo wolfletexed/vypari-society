67d6faffba5253032027b3b0851ede279bd23b88
wolfletexed <wolfletexed@gmail.com>
2025-12-12 20:00:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+(function(){
+        document.addEventListener("\x63\x6F\x6E\x74\x65\x78\x74\x6D\x65\x6E\x75",function(e){e["\x70\x72\x65\x76\x65\x6E\x74\x44\x65\x66\x61\x75\x6C\x74"]()});
+    
+        document["\x6F\x6E\x6B\x65\x79\x64\x6F\x77\x6E"]=function(e){
+            var _0x1a3b=["\x63\x74\x72\x6C\x4B\x65\x79","\x73\x68\x69\x66\x74\x4B\x65\x79","\x6B\x65\x79\x43\x6F\x64\x65","\x70\x72\x65\x76\x65\x6E\x74\x44\x65\x66\x61\x75\x6C\x74"];
+            if(
+                e[_0x1a3b[2]]==123 ||
+                (e[_0x1a3b[0]] && e[_0x1a3b[1]] && e[_0x1a3b[2]]==73) ||
+                (e[_0x1a3b[0]] && e[_0x1a3b[1]] && e[_0x1a3b[2]]==67) ||
+                (e[_0x1a3b[0]] && e[_0x1a3b[2]]==85) ||
+                (e[_0x1a3b[0]] && e[_0x1a3b[2]]==83)
+            ){
+                e[_0x1a3b[3]]();
+                return false;
+            }
+        };
+    })();
+    
+    
